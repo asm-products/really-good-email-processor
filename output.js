20c3ef7259f944d0b0fd6e20f3f426dbc2241424
Wesley Lancel <wesleylancel@gmail.com>
2014-07-03 00:00:17 +0200

    Updated features
    - Gather extra info on email
    - Properly parse Gmail forwards
    - Generate base64

--- output.js
+++ output.js
@@ -1,12 +1,8 @@
 module.exports = function (app) {
     app.get('/output/:id', function (req, res, next) {
         if (app.store[req.params.id]) {
+            // get html from email and wrap in basic html page
             var output = '<html><head><style>* { margin: 0; padding: 0; } body { background-color: white; }</style></head><body>' + app.store[req.params.id] + '</body></html>';
-            
-            output = output.replace("\r", '');
-            output = output.replace("\n", '');
-            output = output.replace('<br><u></u>', '');
-            output = output.replace("<br>\r\n</div></body>", '</div></body');
 
             res.send(output);
         } else res.send(404);
